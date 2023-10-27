7951332c7ee17fdd3c6f247ad16c339c806ae5f6
lauravivan <lauvivang@gmail.com>
2023-10-27 10:36:59 -0300

    Ajuste de responsividade e alteração de layout

--- assets/php/components/navbar.php
+++ assets/php/components/navbar.php
@@ -2,61 +2,64 @@
 $qntProductsTotal = $_SESSION['qntProductsTotal'];
 $userInSession = $_SESSION['userInSession'];
 
-echo "<nav class='navbar navbar-expand-sm bg-dark p-md-5'>
-    <div class='container-fluid'>
-        <a class='navbar-brand text-light m-0 p-0 me-4' href='./index.php'>FunkoMania</a>
-        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#collapsed-navbar' aria-expanded='false' aria-controls='#collapsed-navbar'>
-            <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse' id='collapsed-navbar'>
-            <ul class='navbar-nav d-flex justify-content-between w-100'>
-                <li class='nav-item dropdown'>
-                    <a class='nav-link dropdown-toggle text-light' href='#' role='button' id='navbar-dropdown1' data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                        Produtos
-                    </a>
-                    <ul class='dropdown-menu' aria-labelledby='navbar-dropdown1'>";
+if ($userInSession) { 
+    $href = '././logout.php';
+    $desc = 'Sair';
+} else {
+    $href = '././login.php';
+    $desc = 'Fazer login';
+}
 
+echo "<nav class='navbar navbar-expand-sm bg-dark p-3 p-md-5'>
+        <div class='container-fluid'>
+            <a class='navbar-brand text-light m-0 p-0 me-4 mt-4' href='./index.php'>
+                <img class='logo' src='./assets/img/logo/logo.png'>
+            </a>
+            <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#collapsed-navbar' aria-expanded='false' aria-controls='#collapsed-navbar'>
+                <span class='navbar-toggler-icon'></span>
+            </button>
+            <div class='collapse navbar-collapse' id='collapsed-navbar'>
+                <div class='d-flex flex-column w-100'>";
+if ($userInSession) {
+    $userName = $userInSession['name'];
+    echo "<span class='align-self-sm-end fs-5 welcome fw-bold'>Bem-vinde $userName</span>";
+}
+echo "
+                    <ul class='navbar-nav justify-content-sm-between w-100'>
+                        <li class='nav-item dropdown'>
+                            <a class='nav-link dropdown-toggle text-light' href='#' role='button' id='navbar-dropdown1' data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                Produtos
+                            </a>
+                            <ul class='dropdown-menu' aria-labelledby='navbar-dropdown1'>";                   
 foreach($data as $dt) {
     $category = reset($dt);
     $categoryName = $category->getName();
     echo "<li><a class='dropdown-item' href='./products.php?category=$categoryName'>$categoryName</a></li>";
 }
-
 echo "</ul>
-                </li>
-                <li class='nav-item d-sm-flex column-gap-3'>
-                    <div class='dropdown'>
-                        <a class='nav-link dropdown-toggle text-light' href='#' role='button' id='navbar-dropdown2' data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                            Minha Conta
-                        </a>
-                        <ul class='dropdown-menu' aria-labelledby='navbar-dropdown2'>";
-if ((!empty($userInSession))) {
-    $userName = $userInSession['name'];
-    echo "
-        <li><span class='p-3'>$userName</span></li>
-        <li><a class='dropdown-item' href='././orders.php'>Meus pedidos</a></li>
-        <li><a class='dropdown-item' href='././logout.php'>Sair</a></li>
-    ";
-} else {
-    echo "
-        <li><a class='dropdown-item' href='././login.php'>Fazer Login</a></li>
-        <li><a class='dropdown-item' href='././orders.php'>Meus pedidos</a></li>
-    ";
-}
-echo "</ul>
-                    </div>
-                    <a class='cart mt-2 text-decoration-none' href='./cart.php'>
-                        <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' class='cart-svg'>
-                            <path d='M2.08416 2.7512C2.22155 2.36044 2.6497 2.15503 3.04047 2.29242L3.34187 2.39838C3.95839 2.61511 4.48203 2.79919 4.89411 3.00139C5.33474 3.21759 5.71259 3.48393 5.99677 3.89979C6.27875 4.31243 6.39517 4.76515 6.4489 5.26153C6.47295 5.48373 6.48564 5.72967 6.49233 6H17.1305C18.8155 6 20.3323 6 20.7762 6.57708C21.2202 7.15417 21.0466 8.02369 20.6995 9.76275L20.1997 12.1875C19.8846 13.7164 19.727 14.4808 19.1753 14.9304C18.6236 15.38 17.8431 15.38 16.2821 15.38H10.9792C8.19028 15.38 6.79583 15.38 5.92943 14.4662C5.06302 13.5523 4.99979 12.5816 4.99979 9.64L4.99979 7.03832C4.99979 6.29837 4.99877 5.80316 4.95761 5.42295C4.91828 5.0596 4.84858 4.87818 4.75832 4.74609C4.67026 4.61723 4.53659 4.4968 4.23336 4.34802C3.91052 4.18961 3.47177 4.03406 2.80416 3.79934L2.54295 3.7075C2.15218 3.57012 1.94678 3.14197 2.08416 2.7512Z'></path> 
-                            <path d='M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z'></path>
-                            <path d='M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z'></path>
-                        </svg>
-                        <span class='badge text-bg-secondary' id='badge'>$qntProductsTotal</span>
-                    </a>
-                </li>
-            </ul>
+                        </li>
+                        <li class='nav-item d-sm-flex'>
+                            <div class='dropdown'>
+                                <a class='nav-link dropdown-toggle text-light' href='#' role='button' id='navbar-dropdown2' data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                    Minha Conta
+                                </a>
+                                <ul class='dropdown-menu' aria-labelledby='navbar-dropdown2'>
+                                    <li><a class='dropdown-item' href='$href'>$desc</a></li>
+                                    <li><a class='dropdown-item' href='././orders.php'>Meus pedidos</a></li>
+                                </ul>
+                            </div>
+                            <a class='cart mt-2 text-decoration-none' href='./cart.php'>
+                                <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' class='cart-svg'>
+                                    <path d='M2.08416 2.7512C2.22155 2.36044 2.6497 2.15503 3.04047 2.29242L3.34187 2.39838C3.95839 2.61511 4.48203 2.79919 4.89411 3.00139C5.33474 3.21759 5.71259 3.48393 5.99677 3.89979C6.27875 4.31243 6.39517 4.76515 6.4489 5.26153C6.47295 5.48373 6.48564 5.72967 6.49233 6H17.1305C18.8155 6 20.3323 6 20.7762 6.57708C21.2202 7.15417 21.0466 8.02369 20.6995 9.76275L20.1997 12.1875C19.8846 13.7164 19.727 14.4808 19.1753 14.9304C18.6236 15.38 17.8431 15.38 16.2821 15.38H10.9792C8.19028 15.38 6.79583 15.38 5.92943 14.4662C5.06302 13.5523 4.99979 12.5816 4.99979 9.64L4.99979 7.03832C4.99979 6.29837 4.99877 5.80316 4.95761 5.42295C4.91828 5.0596 4.84858 4.87818 4.75832 4.74609C4.67026 4.61723 4.53659 4.4968 4.23336 4.34802C3.91052 4.18961 3.47177 4.03406 2.80416 3.79934L2.54295 3.7075C2.15218 3.57012 1.94678 3.14197 2.08416 2.7512Z'></path> 
+                                    <path d='M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z'></path>
+                                    <path d='M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z'></path>
+                                </svg>
+                                <span class='badge text-bg-secondary' id='badge'>$qntProductsTotal</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-</nav>";
+    </nav>";
 ?>
-
